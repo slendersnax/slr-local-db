7455fed93d934c11d6293554b3268a72899acd44
Abel Orban <sl.abelorban@gmail.com>
2021-06-30 19:50:56 +0300

    added main classes and base functions for dbs in db class

--- slrLocalDB.js
+++ slrLocalDB.js
@@ -1,5 +1,56 @@
+// text parser --------------------------------------------------------
+class SlrParser {
+    constructor() {
+
+    }
+}
+
+// database handling --------------------------------------------------
 class SlrDB {
     constructor() {
+        sCurDB = "";
+        rDbs = "";
+        rTables = "";
+
+        if(window.localStorage.getItem("slrDBs")) {
+
+        }
+        else {
+            window.localStorage.setItem("slrDBs");
+        }
+    }
+
+    // localStorage wrappers for ease of use
+    set = (key, value) => {
+        window.localStorage.setItem(key, value);
+    }
+
+    get = (key) => {
+        return window.localStorage.getItem(key);
+    }
+
+    remove = (key) => {
+        window.localStorage.removeItem(key);
+    }
+
+    // database functions -------------------------------------------------------------
+    // base functions
+    addDB = (name) => {
+        set(name, "");
+    }
+
+    delDB = (name) => {
+
+    }
+
+    // wrapper functions
+
+    // table functions -----------------------------------------------------------------
+    addTable = () => {
+
+    }
+
+    deleteTable = () => {
 
     }
 }
