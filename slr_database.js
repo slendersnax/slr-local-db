e8f02546bc357eedf4185694c9239a8684d42267
Orban Abel <orban@abel.ro>
2022-04-10 03:34:46 +0300

    modified plans for db and table structure, started implementing them

--- slr_database.js
+++ slr_database.js
@@ -5,19 +5,28 @@ slr_database
 aka everything is a localStorage entry
 
 set-up:
-    - one entry contains the names of the tables
+    - database entry contains the names of the tables
     - each table is a different entry
 
+
+ideas:
+    - maybe make all the functions return something as a form of error signaling?
 */
 
 // database handling --------------------------------------------------
 class SlrDB {
     constructor() {
-        if(window.localStorage.getItem("slr_database")) {
+        this.dbName = "slr_database";
+        this.dbColumnsName = this.dbName + "_column_numbers"
+        this.separator = "/";
+        this.innerSeparator = ";";
+
+        if(window.localStorage.getItem(this.dbName)) {
 
         }
         else {
-            window.localStorage.setItem("slr_database");
+            window.localStorage.setItem(this.dbName, "");
+            window.localStorage.setItem(this.dbColumnsName, "");
         }
     }
 
@@ -34,25 +43,44 @@ class SlrDB {
         window.localStorage.removeItem(key);
     }
 
-    // database functions -------------------------------------------------------------
-    // the only one we really need is resetting the database, deleting everything
-    // 
-    addDB = (name) => {
-        set(name, "");
+    // adding one value to a generic field
+    addToField = (name, content) => {
+        this.set(name, this.get(name) + this.separator + content);
     }
 
-    delDB = (name) => {
-
+    // database functions -------------------------------------------------------------
+    // the only one we really need is resetting the database, deleting everything
+    
+    delDB = () => {
+        // delete database
+        // delete tables details
+        // delete contents of tables
     }
 
     // wrapper functions
 
     // table functions -----------------------------------------------------------------
-    addTable = () => {
-
+    addTable = (name, nOfColumns) => {
+        this.set(name, "");
+        this.addToField(this.dbColumnsName, nOfColumns);
+        
+        this.addToField(this.dbName, name);
+        this.addToField(this.dbName, nOfColumns);
     }
 
-    deleteTable = () => {
+    deleteTable = (name) => {
+        // delete the table entry
+        // also delete its name and number of columns from the tables' names
+    }
 
+    // value functions -----------------------------------------------------------------
+
+    addToTable = (name, ...values) => {
+        if(this.get(name)) {
+
+        }
+        else {
+
+        }
     }
 }
